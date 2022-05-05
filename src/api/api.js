75f7e6ef9bd0a29d0b3afc8ee89a25bc983f1dad
Lucas Aster <asterlc102@gmail.com>
2022-05-05 20:00:32 -0300

    axios default set content-type needed

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,6 @@ const api = axios.create({
     baseURL: `${process.env.RIOT_URL}`,
     headers: {
         "X-Riot-Token": `${process.env.RIOT_KEY}`,
-        "content-type": "charset=utf-8"
     }
 
 });
